4aceb70e80cc6c75dc1b189e5c2f84d76dd35298
Nathan Pawlas <pawlasnr@gmail.com>
2024-08-02 00:35:54 -0400

    Entry Point created

---
+++ includes/gb.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "gb.h"
+
+class gb
+{
+public:
+	bool emu_run(int argc, char* argv[]);
+
+private:
+
+};
---
+++ src/gb.cpp
@@ -0,0 +1,8 @@
+#include "gb.h"
+#include <SDL.h>
+#include <stdio.h>
+
+bool gb::emu_run(int argc, char* argv[])
+{
+	return false;
+}
